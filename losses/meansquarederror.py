0547e04964e203391580762bd73f423c803eb9e3
amirrezarajabi <amir.reza.rajabi13800201@gmail.com>
2023-02-10 02:18:58 -0500

    FEAT mean squared error to implement

---
+++ losses/meansquarederror.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class MeanSquaredError(Loss):
+    def __init__(self):
+        pass
+
+    def compute(self, y_pred, y_true):
+        """
+        computes the mean squared error loss
+            args:
+                y_pred: predicted labels (n_classes, batch_size)
+                y_true: true labels (n_classes, batch_size)
+            returns:
+                mean squared error loss
+        """
+        # TODO: Implement mean squared error loss
+        batch_size = None
+        cost = None
+        return np.squeeze(cost)
+    
+    def backward(self, y_pred, y_true):
+        """
+        computes the derivative of the mean squared error loss
+            args:
+                y_pred: predicted labels (n_classes, batch_size)
+                y_true: true labels (n_classes, batch_size)
+            returns:
+                derivative of the mean squared error loss
+        """
+        # TODO: Implement backward pass for mean squared error loss
+        return None
