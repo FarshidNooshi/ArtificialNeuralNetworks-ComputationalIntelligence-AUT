8833823ab6b4f4ef6045d6107c428aad96ddebad
amirrezarajabi <amir.reza.rajabi13800201@gmail.com>
2023-02-10 06:27:19 -0500

    FEAT model to implement

--- activations.py
+++ activations.py
@@ -1,108 +1,143 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-def sigmoid(Z: np.ndarray) -> np.ndarray:
-    """
-    Sigmoid activation function.
-        args:
-           x: input to the activation function
-        returns:
-            sigmoid(x)
-    """
-    # TODO: Implement sigmoid activation function
-    A = None
-    return A
+class Activation:
+    def __init__(self) -> None:
+        pass
 
-def sigmoid_backward(dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
-    """
-    Backward pass for sigmoid activation function.
-        args:
-            dA: derivative of the cost with respect to the activation
-            Z: input to the activation function
-        returns:
-            derivative of the cost with respect to Z
-    """
-    A = sigmoid(Z)
-    # TODO: Implement backward pass for sigmoid activation function
-    dZ = None
-    return dZ
+    @abstractmethod
+    def forward(self, Z: np.ndarray) -> np.ndarray:
+        """
+        Forward pass for activation function.
+            args:
+                Z: input to the activation function
+            returns:
+                A: output of the activation function
+        """
+        pass
 
-def relu(Z: np.ndarray) -> np.ndarray:
-    """
-    ReLU activation function.
-        args:
+    @abstractmethod
+    def backward(self, dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
+        """
+        Backward pass for activation function.
+            args:
+                dA: derivative of the cost with respect to the activation
+                Z: input to the activation function
+            returns:
+                derivative of the cost with respect to Z
+        """
+        pass
+
+class Sigmoid(Activation):
+    def forward(self, Z: np.ndarray) -> np.ndarray:
+        """
+        Sigmoid activation function.
+            args:
             x: input to the activation function
-        returns:
-            relu(x)
-    """
-    # TODO: Implement ReLU activation function
-    A = None
-    return A
+            returns:
+                sigmoid(x)
+        """
+        # TODO: Implement sigmoid activation function
+        A = None
+        return A
 
-def relu_backward(dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
-    """
-    Backward pass for ReLU activation function.
-        args:
-            dA: derivative of the cost with respect to the activation
-            Z: input to the activation function
-        returns:
-            derivative of the cost with respect to Z
-    """
-    # TODO: Implement backward pass for ReLU activation function
-    dZ = None
-    dZ[Z <= 0] = 0
+    def backward(self, dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
+        """
+        Backward pass for sigmoid activation function.
+            args:
+                dA: derivative of the cost with respect to the activation
+                Z: input to the activation function
+            returns:
+                derivative of the cost with respect to Z
+        """
+        A = self.forward(Z)
+        # TODO: Implement backward pass for sigmoid activation function
+        dZ = None
+        return dZ
+    
 
-    return dZ
+class ReLU(Activation):
+    def forward(self, Z: np.ndarray) -> np.ndarray:
+        """
+        ReLU activation function.
+            args:
+                x: input to the activation function
+            returns:
+                relu(x)
+        """
+        # TODO: Implement ReLU activation function
+        A = None
+        return A
 
-def tanh(Z: np.ndarray) -> np.ndarray:
-    """
-    Tanh activation function.
-        args:
-            x: input to the activation function
-        returns:
-            tanh(x)
-    """
-    # TODO: Implement tanh activation function
-    A = None
-    return A
+    def backward(self, dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
+        """
+        Backward pass for ReLU activation function.
+            args:
+                dA: derivative of the cost with respect to the activation
+                Z: input to the activation function
+            returns:
+                derivative of the cost with respect to Z
+        """
+        # TODO: Implement backward pass for ReLU activation function
+        dZ = None
+        dZ[Z <= 0] = 0
 
-def tanh_backward(dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
-    """
-    Backward pass for tanh activation function.
-        args:
-            dA: derivative of the cost with respect to the activation
-            Z: input to the activation function
-        returns:
-            derivative of the cost with respect to Z
-    """
-    A = tanh(Z)
-    # TODO: Implement backward pass for tanh activation function
-    dZ = None
-    return dZ
+        return dZ
+    
+    
 
-def linear(Z: np.ndarray) -> np.ndarray:
-    """
-    Linear activation function.
-        args:
-            x: input to the activation function
-        returns:
-            x
-    """
-    # TODO: Implement linear activation function
-    A = None
-    return A
+class Tanh(Activation):
+    def forward(self, Z: np.ndarray) -> np.ndarray:
+        """
+        Tanh activation function.
+            args:
+                x: input to the activation function
+            returns:
+                tanh(x)
+        """
+        # TODO: Implement tanh activation function
+        A = None
+        return A
 
-def linear_backward(dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
-    """
-    Backward pass for linear activation function.
-        args:
-            dA: derivative of the cost with respect to the activation
-            Z: input to the activation function
-        returns:
-            derivative of the cost with respect to Z
-    """
-    # TODO: Implement backward pass for linear activation function
-    dZ = None
-    return dZ
+    def backward(self, dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
+        """
+        Backward pass for tanh activation function.
+            args:
+                dA: derivative of the cost with respect to the activation
+                Z: input to the activation function
+            returns:
+                derivative of the cost with respect to Z
+        """
+        A = self.forward(Z)
+        # TODO: Implement backward pass for tanh activation function
+        dZ = None
+        return dZ
+    
+class LinearActivation(Activation):
+    def linear(Z: np.ndarray) -> np.ndarray:
+        """
+        Linear activation function.
+            args:
+                x: input to the activation function
+            returns:
+                x
+        """
+        # TODO: Implement linear activation function
+        A = None
+        return A
+
+    def backward(dA: np.ndarray, Z: np.ndarray) -> np.ndarray:
+        """
+        Backward pass for linear activation function.
+            args:
+                dA: derivative of the cost with respect to the activation
+                Z: input to the activation function
+            returns:
+                derivative of the cost with respect to Z
+        """
+        # TODO: Implement backward pass for linear activation function
+        dZ = None
+        return dZ
 
 def get_activation(activation: str) -> tuple:
     """
@@ -113,13 +148,12 @@ def get_activation(activation: str) -> tuple:
             activation function and its derivative
     """
     if activation == 'sigmoid':
-        return sigmoid, sigmoid_backward
+        return Sigmoid
     elif activation == 'relu':
-        return relu, relu_backward
+        return ReLU
     elif activation == 'tanh':
-        return tanh, tanh_backward
+        return Tanh
     elif activation == 'linear':
-        return linear, linear_backward
+        return LinearActivation
     else:
         raise ValueError('Activation function not supported')
-
---
+++ model.py
@@ -0,0 +1,243 @@
+from layers.convolution2d import Conv2D
+from layers.maxpooling2d import MaxPoll2D
+from layers.fullyconnected import FC
+
+from activations import Activation, get_activation
+
+import pickle
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from random import shuffle
+
+class Model:
+    def __init__(self, arch, criterion, optimizer, name=None):
+        """
+        Initialize the model.
+        args:
+            arch: dictionary containing the architecture of the model
+            criterion: loss 
+            optimizer: optimizer
+            name: name of the model
+        """
+        if name is None:
+            self.model = arch
+            self.criterion = criterion
+            self.optimizer = optimizer
+            self.layers_names = list(arch.keys())
+        else:
+            self.model, self.criterion, self.optimizer, self.layers_names = self.load_model(name)
+    
+    def is_layer(self, layer):
+        """
+        Check if the layer is a layer.
+        args:
+            layer: layer to be checked
+        returns:
+            True if the layer is a layer, False otherwise
+        """
+        # TODO: Implement check if the layer is a layer
+        return None
+
+    def is_activation(self, layer):
+        """
+        Check if the layer is an activation function.
+        args:
+            layer: layer to be checked
+        returns:
+            True if the layer is an activation function, False otherwise
+        """
+        # TODO: Implement check if the layer is an activation
+        return None
+    
+    def forward(self, x):
+        """
+        Forward pass through the model.
+        args:
+            x: input to the model
+        returns:
+            output of the model
+        """
+        tmp = []
+        A = x
+        # TODO: Implement forward pass through the model
+        # NOTICE: we have a pattern of layers and activations
+        for l in range(None):
+            Z = None
+            tmp.append(None)    # hint add a copy of Z to tmp
+            A = None
+            tmp.append(None)    # hint add a copy of A to tmp
+        return A, tmp
+    
+    def backward(self, dAL, tmp, x):
+        """
+        Backward pass through the model.
+        args:
+            dAL: derivative of the cost with respect to the output of the model
+            tmp: list containing the intermediate values of Z and A
+            x: input to the model
+        returns:
+            gradients of the model
+        """
+        dA = dAL
+        grads = {}
+        # TODO: Implement backward pass through the model
+        # NOTICE: we have a pattern of layers and activations
+        # for from the end to the beginning of the tmp list
+        for l in range(None):
+            if l > 2:
+                Z, A = tmp[l - 1], tmp[l - 2]
+            else:
+                Z, A = tmp[l - 1], x
+            dZ = None
+            dA, grads = None
+            grads[self.layers_names[i - 1]] = None
+        return grads
+
+    def update(self, grads):
+        """
+        Update the model.
+        args:
+            grads: gradients of the model
+        """
+        for None:
+            if None:    # hint check if the layer is a layer and also is not a maxpooling layer
+                self.model[None].update(None)
+    
+    def one_epoch(self, x, y):
+        """
+        One epoch of training.
+        args:
+            x: input to the model
+            y: labels
+            batch_size: batch size
+        returns:
+            loss
+        """
+        # TODO: Implement one epoch of training
+        tmp = None
+        AL = tmp[None]
+        loss = None
+        grads = None
+        self.update(None)
+        return loss
+    
+    def save(self, name):
+        """
+        Save the model.
+        args:
+            name: name of the model
+        """
+        with open(name, 'wb') as f:
+            pickle.dump((self.model, self.criterion, self.optimizer, self.layers_names), f)
+        
+    def load_model(self, name):
+        """
+        Load the model.
+        args:
+            name: name of the model
+        returns:
+            model, criterion, optimizer, layers_names
+        """
+        with open(name, 'rb') as f:
+            return pickle.load(f)
+        
+    def shuffle(self, m, shuffling):
+        order = list(range(m))
+        if shuffling:
+            return np.random.shuffle(order)
+        return order
+
+    def batch(self, X, y, batch_size, index, order):
+        """
+        Get a batch of data.
+        args:
+            X: input to the model
+            y: labels
+            batch_size: batch size
+            index: index of the batch
+                e.g: if batch_size = 3 and index = 1 then the batch will be from index [3, 4, 5]
+            order: order of the data
+        returns:
+            bx, by: batch of data
+        """
+        # TODO: Implement batch
+        last_index = None   # hint last index of the batch check for the last batch
+        batch = order[None: None]
+        # NOTICE: inputs are 4 dimensional or 2 demensional
+        if None:
+            bx = None
+            by = None
+            return None, None
+        else:
+            bx = None
+            by = None
+            return None, None
+
+    def compute_loss(self, X, y, batch_size):
+        """
+        Compute the loss.
+        args:
+            X: input to the model
+            y: labels
+            Batch_Size: batch size
+        returns:
+            loss
+        """
+        # TODO: Implement compute loss
+        m = None
+        order = None
+        cost = 0
+        for b in range(m // batch_size):
+            bx, by = None
+            tmp = None
+            AL = None
+            cost += None
+        return cost
+
+    def train(self, X, y, epochs, val=None, batch_size=3, shuffling=False, verbose=1, save_after=None):
+        """
+        Train the model.
+        args:
+            X: input to the model
+            y: labels
+            epochs: number of epochs
+            val: validation data
+            batch_size: batch size
+            shuffling: if True shuffle the data
+            verbose: if 1 print the loss after each epoch
+            save_after: save the model after training
+        """
+        # TODO: Implement training
+        train_cost = []
+        val_cost = []
+        # NOTICE: if your inputs are 4 dimensional m = X.shape[0] else m = X.shape[1]
+        m = None
+        for e in tqdm(1, epochs + 1):
+            order = self.shuffle(None, None)
+            cost = 0
+            for b in range(None):
+                bx, by = None
+                cost += None
+            train_cost.append(None)
+            if val is not None:
+                val_cost.append(None)
+            if verbose != False:
+                if e % verbose == 0:
+                    print("Epoch {}: train cost = {}".format(e, cost))
+                if val is not None:
+                    print("Epoch {}: val cost = {}".format(e, val_cost[-1]))
+        if save_after is not None:
+            self.save(save_after)
+        return train_cost, val_cost
+    
+    def predict(self, X):
+        """
+        Predict the output of the model.
+        args:
+            X: input to the model
+        returns:
+            predictions
+        """
+        # TODO: Implement prediction
+        return None
